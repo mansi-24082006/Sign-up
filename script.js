abdc05883e897151a8c40c2bb363436c74a3163e
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-01-19 08:30:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let popup = document.getElementById('popup');
-function openPopup(){
-    popup.classList.add('open-popup');
+function subscribe(){
+    var email=document.getElementById("email3").value;
+    if(document.getElementById("email3").value==""){
+        document.getElementById("sub").disabled;
+        document.getElementById("errorm").classList.add("error1");
+        document.getElementById("errorm").classList.remove("error");
+    }
+   
+   else if(email.indexOf('@')===-1 || email.indexOf('.')===-1){
+    document.getElementById("sub").disabled;
+    document.getElementById("errorm").innerHTML="please include @ and . in email"
+    document.getElementById("errorm").classList.add("error1");
+    document.getElementById("errorm").classList.remove("error");
+    return false;
+   }
+    else{
+    let email=document.getElementById("email3").value;
+    document.getElementById("emailu").innerHTML = email;
+    document.getElementById("signup").classList.add("remove");
+    document.getElementById("sucess").classList.add("cardsucess1");
+    document.getElementById("sucess").classList.remove("cardsucess");
+    }
 }
-function closePopup(){
-    popup.classList.remove('open-popup');
+function dismiss(){
+    document.getElementById("signup").classList.remove("remove");
+    document.getElementById("sucess").classList.remove("cardsucess1");
+    document.getElementById("sucess").classList.add("cardsucess");
 }
